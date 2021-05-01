f6fde2ae492d2bcb4659d90dc2c896a82ab616e9
tonyfan <tonyfan2018@gmail.com>
2021-04-30 21:32:10 -0400

    add grads.html grad.js and add new style.css

---
+++ js/background.js
@@ -0,0 +1,87 @@
+var frame = new Frame({
+    scaling: "full",
+    captureMouse: true,
+    rollover: true,
+    allowDefault: true
+});
+
+frame.on("ready", () => {
+    zog("ready from ZIM Frame");
+
+    const stage = frame.stage;
+    const stageW = frame.width - 100;
+    const stageH = frame.height
+
+    var r1;
+    var r2;
+    if(stageW < 1200 && stageW > 700){
+        r1 = 120;
+        r2 = 40;
+    }else if(stageW <= 700){
+        r1 = 100;
+        r2 = 32;
+    }else{
+        r1 = 150;
+        r2 = 50;
+    }
+
+    var posX = series(0,0,30,30,30,30);
+    var posY = series(-130, -130, 0, 0, 30, 30);
+    var posH = series(CENTER,CENTER, LEFT,LEFT, RIGHT,RIGHT);
+    var posV = series(TOP,TOP, CENTER,CENTER, BOTTOM, BOTTOM );
+    var movs = series(230, 80, 200);
+    var rots = series(30, 270, 150);
+
+    loop(3, (i) => {
+        const bigCircle_i = new Circle(r1,new GradientColor(["rgba(251,175,150,.3)","rgba(122,78,198,.3)"],[0,1], -150,0, 0,150))
+            .pos(posX(),posY(),posH(),posV());
+        const smallCircle_i = new Circle(r2,new GradientColor(["rgba(122,78,198,.5)","rgba(122,78,198,.5)"],[0,1], -30,0, 0, 30))
+            .pos(posX(),posY(),posH(),posV())
+            .mov(-20,movs())
+            .rot(rots());
+    })
+
+    const sink = new Circle(10, pink)
+        .pos(0, 200, LEFT, CENTER)
+        .alp(0)
+        .wiggle("x", stageW / 2, 200, 500, 1000, 2000)
+        .wiggle("y", stageH / 2, 200, 200, 1000, 2000);
+
+    stage.on("stagemousemove", () => {
+        sink.stopAnimate();
+    }, null, true);
+
+
+    stage.on("stagemousemove", () => {
+        sink.loc(frame.mouseX, frame.mouseY);
+    });
+
+
+    const emitter = new Emitter({
+            gravity: 0,
+            obj: {
+                type: "shape",
+                s: ["rgba(251,175,150,.5)", "rgba(122,78,198,.5)"],
+                ss: 0.5
+            },
+            sink: sink,
+            sinkForce: 6, // force towards sink
+            force: 1, // initial force
+            angle: -90, // start all particles pointing up
+            life: 4000, // last over 4 seconds
+            decayTime: 2000 // fade out over the last 2 seconds
+        })
+        .center()
+        .alp(0.1)
+        .mov(0, -600);
+
+    stage.on("stagemousedown", () => {
+        emitter.pauseEmitter(!emitter.emitterPaused, null, true);
+    });
+
+
+}); // end of ready
+
+frame.canvas.style.position = "absolute";
+frame.canvas.style.zIndex = -10;
+// frame.canvas.style.pointerEvents = "none";
---
+++ js/carousel.js
@@ -0,0 +1,24 @@
+
+var caroselArea = document.getElementById("carouselArea");
+
+
+$.getJSON("./js/openHouseProjects_array.json", function (json) {
+    console.log(json); // this will show the info it in firebug console
+
+    let projectsDivs = " ";
+
+    var i;
+    for (i = 0; i < json.length; i++) {
+        var active = " "
+
+        if (i <= 0) active = 'active';
+
+        projectsDivs += "<div class='carousel-item col-md-4 " + active +"'> <div class='d-block w-30 projectBox'> <div class='projectThumbnail' style='background-image: url(" + json[i].projectThumbnail + "); '  > </div><h2 class='projectTitle'>" + json[i].projectName + "</h2><p class='projectNames'>" + json[i].projectMembers + "</p><p class='projectDetails'>" + json[i].projectBlurb + "</p><a href='" + json[i].projectURL + "' class='projectLink'> View Project</a></div></div>"
+
+    }
+
+    console.log(projectsDivs);
+    carouselArea.innerHTML = projectsDivs;
+
+});
+
