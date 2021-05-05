780ef698fd5fba55f4258fa6bc7a8501c56b0dc4
thisioke <seyitanoke@gmail.com>
2021-05-05 12:24:14 -0400

    Fixed work carosel to display view project better at all acreen sizes

--- js/carousel2.js
+++ js/carousel2.js
@@ -16,7 +16,7 @@ $.getJSON("./js/openHouseProjects_array.json", function (json) {
 
         if (i <= 0) active = 'active';
 
-        projectsDivs += "<div class='carousel-item col-12 col-sm-6 col-md-4 col-lg-3 " + active + "'> <div class='d-block w-30 projectBox'> <div class='projectThumbnail' style='background-image: url(" + json[i].projectThumbnail + "); '  > </div><h2 class='projectTitle'>" + json[i].projectName + "</h2><p class='projectNames'>" + json[i].projectMembers + "</p><p class='projectDetails'>" + json[i].projectBlurb + "</p><a href='" + json[i].projectURL + "' class='projectLink'> View Project</a></div></div>"
+        projectsDivs += "<div class='carousel-item col-12 col-sm-6 col-md-4 col-lg-3 .col-xl-3 " + active + "'> <div class='d-block w-30 projectBox'> <div class='projectThumbnail' style='background-image: url(" + json[i].projectThumbnail + "); '  > </div><h2 class='projectTitle'>" + json[i].projectName + "</h2><p class='projectNames'>" + json[i].projectMembers + "</p><p class='projectDetails'>" + json[i].projectBlurb + "</p><a href='" + json[i].projectURL + "' class='projectLink'> View Project</a></div></div>"
 
     }
 
